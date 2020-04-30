9406909f8201cc2483019e4f4d4231276c96ea52
yuningyang19 <51691872+yuningyang19@users.noreply.github.com>
2020-04-30 11:07:45 +0800

    updated

--- tensor_generating/generate_nonnegative_sparse_tensor.m
+++ tensor_generating/generate_nonnegative_sparse_tensor.m
@@ -1,27 +1,20 @@
 function [A] = generate_nonnegative_sparse_tensor(n,d,sparse_level)
 
-   t=[];
-   for i = 1: d
-       t = [t n];
-   end
-   A = tensor(rand(t));  A = double(symmetrize(A)); 
-   
-   is_true = 1;
-   mu = 0.5;
-   while is_true
-       
-      b=sum(A(:)>mu);
-      if b/n^d > sparse_level
-          mu = mu + 0.001;
-      else
-          is_true = 0;
-      end
-      
-      
-       
-   end
-   
-   A = A.* double(A>mu);
+t = ones(1,d); t = t*n;
+A =   rand(t);
+all_per = perms(1:d); A = symmetrization(A,all_per);
+
+is_true = 1;
+mu = 0.5;
+while is_true
+b=sum(A(:)>mu);
+if b/n^d > sparse_level
+mu = mu + 0.001;
+else
+is_true = 0;
+end
+end
+A = A.* double(A>mu);
 
 
 
--- utils/symmetrization.m
+++ utils/symmetrization.m
@@ -1,6 +1,6 @@
 function [sX] = symmetrization(X,all_per)
-
-
+% X  - data tensor
+% all_per - all the permutations of [1,2,...,d]
     sX = 0;
     len_per = length(all_per);
     for i = 1:len_per
